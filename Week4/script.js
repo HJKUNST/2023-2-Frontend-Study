58c5aeda86a67122d0735913e78e9f83a80f5eaf
김희주 <kimheejoo@gimhuijuui-MacBookPro-2.local>
2023-10-11 15:49:33 +0900

    Week4 과제제출

--- Week4/script.js
+++ Week4/script.js
@@ -1,10 +1,3 @@
-function appendToDisplay(value) {
-    document.getElementById('display').value += value;
-}
-
-function clearDisplay() {
-    document.getElementById('display').value = '';
-}
 
 function calculateResult() {
     try {
