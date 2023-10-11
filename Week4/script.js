88ff53343cd0a53cdcca158ee5bb0feae22048f0
HJKUNST <127034324+HJKUNST@users.noreply.github.com>
2023-10-11 15:59:52 +0900

    Delete Week4 directory

--- Week4/script.js
+++
@@ -1,9 +0,0 @@
-
-function calculateResult() {
-    try {
-        let result = eval(document.getElementById('display').value);
-        document.getElementById('display').value = result;
-    } catch (error) {
-        document.getElementById('display').value = 'Error';
-    }
-}
